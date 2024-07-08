9181b12092c65a388793b4deebadfc42d3a52293
MinhyeokChoco <smile_forever@nate.com>
2024-07-08 20:14:12 +0900

    [docs] : 20240707 U, D 코드 수정

--- 20240704/routers/post.router.js
+++ 20240704/routers/post.router.js
@@ -9,4 +9,11 @@ router.post("/create", upload.single("image"), (req, res) => postController.crea
 
 router.get("/view", (req, res) => postController.findPostAll(req, res));
 
+router.get("/update", (req, res) => res.render("update"));
+router.post("/update", upload.single("image"), (req, res) => postController.updatePost(req, res));
+
+router.get("/view", (req, res) => postController.deletePost(req, res));
+
+
+
 module.exports = router;
